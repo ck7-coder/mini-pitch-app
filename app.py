d286d349209c9acf5f0ae29d91e40b9613ebaed0
Chrishan Kanagalingam <kanagalingamchrishan@gmail.com>
2026-09-13 20:11:13 +0100

    Change last updated date to 8 September 2026
    Updated the last updated date in the privacy policy.

--- app.py
+++ app.py
@@ -214,7 +214,7 @@ Karman Holdings 10-K Balance Sheet
 
     """,
     "Privacy Policy": """
-**Last updated: 8 September 2025**
+**Last updated: 8 September 2026**
 
 We at Mini-Pitch, consider your privacy to be something of the utmost importance. The details listed down below note how we: collect, use, store, and protect information when you use our website. As transparency is core to our key values, the details of what we do not collect and the measures we take to ensure your data remains safe is also noted below.
 By using our website, you agree to the terms of this Privacy Policy.
